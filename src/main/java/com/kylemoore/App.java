01e701fad8ec0327b967b7817f7eea10548ab51f
Kyle Moore <kmoore@gradle.com>
2024-05-04 10:14:22 +0200

    Stub project
    Generated from `mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4`

---
+++ src/main/java/com/kylemoore/App.java
@@ -0,0 +1,13 @@
+package com.kylemoore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
